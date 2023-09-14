56b8b6b92cde8a3cf0305959c3f2785bc51d15dc
Ceri <ceriwoolway@gmail.com>
2023-09-14 20:31:16 +0100

    Scrap and rewrite React app

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,28 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import getRandomPokemon from './getRandomPokemon';
+import samplePokemon from './sample-pokemon.json';
 
-type Pokemon = {
+export type Pokemon = {
   name: string;
-  id: string;
-  sprites: {
-    other: {
-      'official-artwork': {
-        front_default: string;
-      };
-    };
-  };
+  [key: string]: any;
 };
 
-const emptyPokemon: Pokemon = {
-  name: '',
-  id: '',
-  sprites: {
-    other: {
-      'official-artwork': {
-        front_default: '',
-      },
-    },
-  },
-};
-
-interface PokemonContextValue {
-  pokemon: Pokemon | null;
-  updatePokemonName: () => Promise<void>;
-}
-
-const PokemonContext = createContext<PokemonContextValue>({
-  pokemon: null,
-  updatePokemonName: async () => {},
-});
-
-const updatePokemon = async () => {
-  const randomNumber = Math.floor(Math.random() * 1010);
-  try {
-    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
-    const response = await fetch(apiUrl);
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error('Error:', error);
-    return { name: '' }; // Return an empty object in case of an error
-  }
-};
-
-const PokemonProvider = ({ children }: { children: React.ReactNode }) => {
-  const [pokemon, setPokemon] = useState(emptyPokemon);
-
-  const updatePokemonName = async () => {
-    const newPokemon = await updatePokemon();
-    setPokemon(newPokemon);
-  };
+export default function App() {
+  const [pokemon, setPokemon] = useState(samplePokemon as Pokemon);
 
   useEffect(() => {
-    updatePokemonName();
+    async function fetchPokemon() {
+      try {
+        const myPokemon = await getRandomPokemon();
+        console.log(myPokemon);
+        setPokemon(myPokemon);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+    fetchPokemon();
   }, []);
 
-  return <PokemonContext.Provider value={{ pokemon, updatePokemonName }}>{children}</PokemonContext.Provider>;
-};
-
-const PokemonCard = () => {
-  const { pokemon } = useContext<PokemonContextValue>(PokemonContext);
-
-  return (
-    <div>
-      <h2>{pokemon?.name}</h2>
-      <div>#{pokemon?.id}</div>
-      <img className="pokemon-img" src={pokemon?.sprites.other['official-artwork']['front_default']} alt="" />
-    </div>
-  );
-};
-
-function App() {
-  return (
-    <PokemonProvider>
-      <div className="App">
-        <PokemonCard />
-        <PokemonContext.Consumer>
-          {(pokemonContext) => (
-            <button id="new-pokemon-button" onClick={() => pokemonContext.updatePokemonName()}>
-              Click me
-            </button>
-          )}
-        </PokemonContext.Consumer>
-      </div>
-    </PokemonProvider>
-  );
+  return <div>{pokemon.name}</div>;
 }
-
-export default App;
---
+++ src/getRandomPokemon.ts
@@ -0,0 +1,22 @@
+import { Pokemon } from './App';
+import samplePokemon from './sample-pokemon.json';
+
+const getRandomPokemon = (): Promise<Pokemon> => {
+  const randomNumber = Math.floor(Math.random() * 1010);
+  const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
+
+  return fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json() as unknown as Pokemon; // Cast the result as Pokemon
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      // return undefined; // Return undefined in case of an error
+      return samplePokemon as Pokemon;
+    });
+};
+
+export default getRandomPokemon;
